76c302074ed325f5d717225c14970985b1ff3cce
terygasmen <terygasmen@gmail.com>
2023-01-27 13:17:52 +0800

    created index file

---
+++ phpmotors/index.php
@@ -0,0 +1,15 @@
+<?php
+
+// This is the main controller
+$action = filter_input(INPUT_POST, 'action');
+    if ($action == NULL){
+    $action = filter_input(INPUT_GET, 'action');
+}
+
+switch ($action){
+    case 'template':
+        include 'view/template.php';
+        break;
+    default:
+        include 'view/home.php';
+}
