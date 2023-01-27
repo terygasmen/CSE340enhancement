15a002639e5d610e5df3e876e362b25fede8106e
terygasmen <terygasmen@gmail.com>
2023-01-27 13:39:39 +0800

    built a dynamic navigation

--- phpmotors/index.php
+++ phpmotors/index.php
@@ -1,6 +1,23 @@
 <?php
 
 // This is the main controller
+
+// Get the database connection file
+require_once 'library/connections.php';
+// Get the main model for use as needed
+require_once 'model/main-model.php';
+
+// Get the array of classifications from DB using model
+$classifications = getClassifications();
+
+// Build a navigation bar using the $classifications array
+$navList = '<ul>';
+$navList .= "<li><a href='/phpmotors/index.php' title='View the PHP Motors home page'>Home</a></li>";
+foreach($classifications as $classification) {
+    $navList .= "<li><a href='/phpmotors/index.php? action=" . urlencode($classification['classificationName']). "' title='View our $classification[classificationName] product line'>$classification[classificationName]</a></li>";
+}
+$navList .= '</ul>';
+
 $action = filter_input(INPUT_POST, 'action');
     if ($action == NULL){
     $action = filter_input(INPUT_GET, 'action');
